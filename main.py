81b310a6f0eb7c217b81ae0dfc556fb76642bf9a
Donatas Noreika <donoras@gmail.com>
2023-07-26 08:18:03 +0300

    +Kai skaičius atspėtas, pranešti apie tai ir baigti programą

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import random
 sugeneguotas = random.randint(1, 101)
 print(sugeneguotas)
 while True:
-    spejimas = int(input("Spėkite skaičių: "))
+    spejimas = int(input("Spėkite skaičių nuo 0 iki 100: "))
     if spejimas > sugeneguotas:
         print("Mažiau")
     if spejimas < sugeneguotas:
         print("Daugiau")
+    if spejimas == sugeneguotas:
+        print(f"Atspėjote! Skaičius - {sugeneguotas}")
+        break
