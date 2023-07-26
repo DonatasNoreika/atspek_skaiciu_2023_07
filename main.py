e6b09825fcc9f7a65cac4af182fa1a1b64461da5
Donatas Noreika <donoras@gmail.com>
2023-07-26 08:37:24 +0300

    +Rodyti likusių spėjimų diapazoną

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import random
 print("Įveskite spėjamo skaičiaus diapazoną: ")
 nuo = int(input("Nuo: "))
 iki = int(input("Iki: "))
+spejamas_nuo = nuo
+spejamas_iki = iki
 sugeneguotas = random.randint(nuo, iki)
 spejimai = 0
 while True:
-    spejimas = int(input(f"Spėkite skaičių nuo {nuo} iki {iki}: "))
+    spejimas = int(input(f"Spėkite skaičių nuo {spejamas_nuo} iki {spejamas_iki}: "))
     spejimai += 1
     if spejimas > sugeneguotas:
+        spejamas_iki = spejimas
         print("Mažiau")
     if spejimas < sugeneguotas:
+        spejamas_nuo = spejimas
         print("Daugiau")
     if spejimas == sugeneguotas:
         print(f"Atspėjote! Skaičius - {sugeneguotas}")
